b32fe468c010f69ce045be49d1098ce15eafd304
Brian Burns <bdburns6389@gmail.com>
2020-10-19 13:23:17 -0500

    Form posting back to parent component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import Form from './Form';
-import './App.css';
+import React, { useState } from "react";
+import Form from "./Form";
+import GoogleAuth from "./components/GoogleAuth";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <Form />
-    </div>
-  );
+	const [isSignedIn, setIsSignedIn] = useState(null);
+
+	const handleAuth = (input) => {
+		setIsSignedIn(input);
+	};
+
+	const handleSubmit = (e, input) => {
+		e.preventDefault();
+
+		// Do stuff with input.
+	};
+
+	return (
+		<div>
+			<GoogleAuth handleAuth={handleAuth} isSignedIn={isSignedIn} />
+			<Form handleSubmit={handleSubmit} />
+		</div>
+	);
 }
 
 export default App;
--- src/Form.js
+++ src/Form.js
@@ -1,57 +1,69 @@
-import React from 'react';
-import { useInputChange } from './useInputChange';
+import React from "react";
+import { useInputChange } from "./helpers/useInputChange";
 
-const Form = () => {
-    const [input, handleInputChange] = useInputChange();
+const Form = (props) => {
+	const [input, handleInputChange] = useInputChange();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(input);
-    }
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		props.handleSubmit(e, input);
+	};
 
-    return (
-        <form onSubmit={handleSubmit} className="flex">
-            <div>
-                <label>Date:</label>
-                <input type="text" name="date" onChange={handleInputChange} />
-            </div>
-            <div>
-                <label>Instructor:</label>
-                <input type="text" name="instructor" onChange={handleInputChange} />
-            </div>
-            <div>
-                <label>Student Name:</label>
-                <input type="text" name="student-name" onChange={handleInputChange} />
-            </div>
-            <div>
-                <label>Student Age:</label>
-                <input type="text" name="student-age" onChange={handleInputChange} />
-            </div>
-            <div>
-                <label>Student Skill Level:</label>
-                <input type="text" name="student-skill-level" onChange={handleInputChange} />
-            </div>
-            <div>
-                <label>Skill Worked On:</label>
-                <input type="text" name="skill-worked-on" onChange={handleInputChange} />
-            </div>
-            <div>
-                <label>Skill Improved?</label>
-                <input type="text" name="skill-improved" onChange={handleInputChange} />
-            </div>
-            <div>
-                <label>Why or Why Not?</label>
-                <input type="text" name="why-or-why-not" onChange={handleInputChange} />
-            </div>
-            <div>
-                <label>Additional Notes:</label>
-                <input type="text" name="additional-notes" onChange={handleInputChange} />
-            </div>
-            <div>
-                <input type="submit" />
-            </div>
-        </form>
-    )
+	return (
+		<form onSubmit={handleSubmit} className="flex">
+			<div>
+				<label>Date:</label>
+				<input type="text" name="date" onChange={handleInputChange} />
+			</div>
+			<div>
+				<label>Instructor:</label>
+				<input type="text" name="instructor" onChange={handleInputChange} />
+			</div>
+			<div>
+				<label>Student Name:</label>
+				<input type="text" name="student-name" onChange={handleInputChange} />
+			</div>
+			<div>
+				<label>Student Age:</label>
+				<input type="text" name="student-age" onChange={handleInputChange} />
+			</div>
+			<div>
+				<label>Student Skill Level:</label>
+				<input
+					type="text"
+					name="student-skill-level"
+					onChange={handleInputChange}
+				/>
+			</div>
+			<div>
+				<label>Skill Worked On:</label>
+				<input
+					type="text"
+					name="skill-worked-on"
+					onChange={handleInputChange}
+				/>
+			</div>
+			<div>
+				<label>Skill Improved?</label>
+				<input type="text" name="skill-improved" onChange={handleInputChange} />
+			</div>
+			<div>
+				<label>Why or Why Not?</label>
+				<input type="text" name="why-or-why-not" onChange={handleInputChange} />
+			</div>
+			<div>
+				<label>Additional Notes:</label>
+				<input
+					type="text"
+					name="additional-notes"
+					onChange={handleInputChange}
+				/>
+			</div>
+			<div>
+				<input type="submit" />
+			</div>
+		</form>
+	);
 };
 
 export default Form;
