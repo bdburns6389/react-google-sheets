aec2cc2ab4cebefad2aede63e8aa39702d749bd9
Brian Burns <bdburns6389@gmail.com>
2020-12-19 17:31:53 -0600

    Changed variable

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,73 +1,74 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
-  // Array of API discovery doc URLs for APIs used by the quickstart
-  DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+	// Array of API discovery doc URLs for APIs used by the quickstart
+	DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
 
-  componentDidMount() {
-    // Initialize OAuth2 library.
-    window.gapi.load("client:auth2", () => {
-      window.gapi.client
-        .init({
-          apiKey: "AIzaSyCRGMUcELLJ8paWzb67ORZs67bpqR73bDI",
-          clientId: process.env.REACT_APP_CLIENT_ID,
-          scope: "email https://www.googleapis.com/auth/spreadsheets",
-          discoveryDocs: this.DISCOVERY_DOCS,
-        })
-        .then(() => {
-          // Promise that is called when init is sucessfully returned.
-          this.auth = window.gapi.auth2.getAuthInstance();
-          this.props.handleAuth(this.auth.isSignedIn.get());
-          this.auth.isSignedIn.listen(this.onAuthChange);
-        });
-    });
-  }
+	componentDidMount() {
+		// Initialize OAuth2 library.
+		window.gapi.load("client:auth2", () => {
+			window.gapi.client
+				.init({
+					apiKey: "AIzaSyDE_rk2RlmOXOtPOmUJmVy55t4CRbg2iZI",
+					clientId:
+						"955206631296-vtacv5rv74skhu27i0si1u65bnsvha4e.apps.googleusercontent.com",
+					scope: "email https://www.googleapis.com/auth/spreadsheets",
+					discoveryDocs: this.DISCOVERY_DOCS,
+				})
+				.then(() => {
+					// Promise that is called when init is sucessfully returned.
+					this.auth = window.gapi.auth2.getAuthInstance();
+					this.props.handleAuth(this.auth.isSignedIn.get());
+					this.auth.isSignedIn.listen(this.onAuthChange);
+				});
+		});
+	}
 
-  // Get access token while signed in.
-  // window.gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token
+	// Get access token while signed in.
+	// window.gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token
 
-  // Get current user's id.
-  // gapi.auth2.getAuthInstance().currentUser.get().getId();
+	// Get current user's id.
+	// gapi.auth2.getAuthInstance().currentUser.get().getId();
 
-  onAuthChange = () => {
-    this.props.handleAuth(this.auth.isSignedIn.get());
-  };
+	onAuthChange = () => {
+		this.props.handleAuth(this.auth.isSignedIn.get());
+	};
 
-  onSignIn = () => {
-    this.auth.signIn();
-  };
+	onSignIn = () => {
+		this.auth.signIn();
+	};
 
-  onSignOut = () => {
-    this.auth.signOut();
-  };
+	onSignOut = () => {
+		this.auth.signOut();
+	};
 
-  renderAuthButton() {
-    if (this.props.isSignedIn === null) {
-      return null;
-    } else if (this.props.isSignedIn) {
-      return (
-        <button className="btn btn-auth" onClick={this.onSignOut}>
-          <i className="" />
-          Sign Out
-        </button>
-      );
-    } else {
-      return (
-        <button className="btn btn-auth" onClick={this.onSignIn}>
-          <i className="" />
-          Sign In With Google
-        </button>
-      );
-    }
-  }
+	renderAuthButton() {
+		if (this.props.isSignedIn === null) {
+			return null;
+		} else if (this.props.isSignedIn) {
+			return (
+				<button className="btn btn-auth" onClick={this.onSignOut}>
+					<i className="" />
+					Sign Out
+				</button>
+			);
+		} else {
+			return (
+				<button className="btn btn-auth" onClick={this.onSignIn}>
+					<i className="" />
+					Sign In With Google
+				</button>
+			);
+		}
+	}
 
-  render() {
-    return (
-      <div>
-        <div>{this.renderAuthButton()}</div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div>{this.renderAuthButton()}</div>
+			</div>
+		);
+	}
 }
 
 export default GoogleAuth;
